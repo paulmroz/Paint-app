971ef66d2a92bb6a714f8cba6e81c487e039b63b
Paweł Mróz <paulmroz97@gmail.com>
2019-10-25 22:45:13 +0200

    First milestone

--- PaintApp.pro
+++ PaintApp.pro
@@ -4,9 +4,11 @@
 #
 #-------------------------------------------------
 
-QT       += core gui
+QT       += core gui printsupport
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+
 
 TARGET = PaintApp
 TEMPLATE = app
@@ -25,10 +27,12 @@ DEFINES += QT_DEPRECATED_WARNINGS
 
 SOURCES += \
         main.cpp \
-        mainwindow.cpp
+        mainwindow.cpp \
+    scribblearea.cpp
 
 HEADERS += \
-        mainwindow.h
+        mainwindow.h \
+    scribblearea.h
 
 FORMS += \
         mainwindow.ui
--- mainwindow.h
+++ mainwindow.h
@@ -1,24 +1,63 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QList>
 #include <QMainWindow>
 
-namespace Ui {
-class MainWindow;
-}
+
+class ScribbleArea;
 
 class MainWindow : public QMainWindow
 {
+
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
-    //test
-    //test2
+    MainWindow();
+
+protected:
+
+    void closeEvent(QCloseEvent *event) override;
+
+
+private slots:
+    void open();
+    void save();
+    void penColor();
+    void penWidth();
+    void about();
 
 private:
-    Ui::MainWindow *ui;
+
+    void createActions();
+    void createMenus();
+
+
+    bool maybeSave();
+
+
+    bool saveFile(const QByteArray &fileFormat);
+
+
+    ScribbleArea *scribbleArea;
+
+
+    QMenu *saveAsMenu;
+    QMenu *fileMenu;
+    QMenu *optionMenu;
+    QMenu *helpMenu;
+
+
+    QAction *openAct;
+
+    QList<QAction *> saveAsActs;
+    QAction *exitAct;
+    QAction *penColorAct;
+    QAction *penWidthAct;
+    QAction *printAct;
+    QAction *clearScreenAct;
+    QAction *aboutAct;
+    QAction *aboutQtAct;
 };
 
-#endif // MAINWINDOW_H
+#endif
